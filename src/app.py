3919c99b7ef65f3ba48b1ae5e6a500072f1fc0d7
Tastypotato245 <rbt2775@cau.ac.kr>
2024-03-10 15:48:28 +0900

    modi: dir pos and problem text I to he

--- src/app.py
+++ src/app.py
@@ -1,13 +1,15 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Flask, render_template, request, jsonify
 from openai import OpenAI
 
 import os
 
 app = Flask(__name__)
-CORS(app)
 
 API_KEY = os.getenv("FLASK_API_KEY")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/chat', methods=['POST'])
 def chat():
